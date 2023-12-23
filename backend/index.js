ecf85920e46d7fc82ba5012fe0ebd3457e5bac7f
Kiran kuyate <84271800+kirankuyate2157@users.noreply.github.com>
2023-12-23 20:53:54 +0530

    origin added

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,13 @@ const connectDB = async () => {
 dotenv.config();
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://blog-ska7.onrender.com"],
+    credentials: true,
+  })
+);
+
 app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
