55a81c4a158748e675f70879e05b6015481804b2
Kiran kuyate <84271800+kirankuyate2157@users.noreply.github.com>
2023-12-24 14:02:23 +0530

    corns updated  ..

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 app.use(
   cors({
-    origin: ["*"],
+    origin: "*",
     credentials: true,
   })
 );
